af7552378b414073ab0afcbc54468d0765ee8733
euchigere <ugochigere02@gmail.com>
2020-05-09 18:59:08 +0000

    Update forms and routes files

--- app/forms.py
+++ app/forms.py
@@ -32,3 +32,13 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data.capitalize() != self.original_username:
+            user = User.query.filter_by(username=self.username.data.capitalize()).first()
+            if user is not None:
+                raise ValidationError('Please use adifferent username')
